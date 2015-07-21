67b7adebb3c2c0b21d3759419c781a6bed8353f9
Drew DeVault <ddevault@linode.com>
2015-07-21 11:46:55 -0400

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo "We are going to tweak this template for your project's needs."
+
+echo -n "What npm package name do you want to use: "
+read package_name
+
+sed -E "s/reactjs-starter-kit/$package_name/g" package.json > .package.json
+mv .package.json package.json
+
+echo "I'll leave the rest of package.json to you."
+
+echo -n "What git origin do you want to use: "
+read git_origin
+
+git remote rm origin
+git remote add origin "$git_origin"
+
+rm bootstrap.sh
+
+git add -A
+git commit -m "Bootstrap react starter kit into $package_name"
+
+echo "Okay, you're good to go."
+
+echo -n "Do you want to push (y/n)? "
+read do_push
+
+if [ "$do_push" == "y" ]
+then
+    git push -u origin master
+fi
